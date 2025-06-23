1c63b2b77a3754722b55dd42253f5d7df7ea4a21
EnzoCasalini <enzocasa83@gmail.com>
2025-06-23 03:48:57 +0200

    AURA

--- server/main.py
+++ server/main.py
@@ -18,7 +18,11 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "https://enzocasalini.github.io"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
